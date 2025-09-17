e99c76c6dd80f18070a80379737f494ff04e34a9
Yulylm <yulyanalaura@gmail.com>
2025-09-16 20:37:28 -0500

    Ignorar archivos

---
+++ config/database.local.php
@@ -0,0 +1,10 @@
+<?php
+// config/database.local.php.example
+return [
+    'host'    => 'localhost',
+    'user'    => 'root',
+    'password'=> '',
+    'dbname'  => 'frfcp_calificaciones',
+    'port'    => 3306,
+    'charset' => 'utf8mb4',
+];
